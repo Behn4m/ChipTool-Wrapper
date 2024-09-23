1e161faa2ffa016800084b95bd88201986ace888
Behn4m <behnam.shakibafar@gmail.com>
2024-09-23 00:19:18 -0400

    Comissioning implemented

--- ChipWrapper.cpp
+++ ChipWrapper.cpp
@@ -1,34 +1,128 @@
+#include <cstdlib> 
 #include "ChipWrapper.h"
 #include <iostream>
 #include <string>
 
-
+/**
+ * @brief Constructor for the ChipWrapper class.
+ */
 ChipWrapper::ChipWrapper()
 {
 
 }
 
+/**
+ * @brief Destructor for the ChipWrapper class.
+ */
 ChipWrapper::~ChipWrapper()
 {
     
 }
 
+/**
+ * @brief Initializes the chip.
+ */
 bool ChipWrapper::Initialize()
 {
-    std::cout << "starting initialization ...\n";
+    ChipWrapper::CommissionDevice(0x7283);
     return true;
 }
 
-bool ChipWrapper::SendCommand(const int deviceID, const std::string& command)
+/**
+ * @brief Sends a command to the specified device.
+ */
+bool ChipWrapper::CommissionDevice(const int deviceID)
 {
+    std::cout << "starting initialization ...\n";
+    char buffer[128];
 
-//./chip-tool pairing ble-wifi 0x7283 <ssid> <passphrase> 20202021 3840
+    std::string command;
+    std::string ssid;
+    std::string passphrase;
+
+    command = "~/chip-tool pairing ble-wifi " + std::to_string(deviceID) + " " + getSSID() + " " + getPassphrase() + " " + "20202021 3840";
+
+    std::cout << "command is: " << command << std::endl;
+
+    int result = system(command.c_str());
+    
+    if (result)
+    {
+        std::cout << "failed to commision device";
+        return false;
+    }
+
+    std::cout << "device successfully commisioned";
+    return true;
 }
 
+/**
+ * @brief Gets the status of the specified device.
+ */
 std::string ChipWrapper::GetDeviceStatus(const int deviceID)
 {
     std::cout << "getting " << std::to_string(deviceID) << "deivce status\n";
 
     std::string status = "connected";
     return status;
+}
+
+/**
+ * @brief Retrieves the SSID.
+ */
+std::string ChipWrapper::getSSID()
+{
+    char buffer[128];
+
+    std::string ssid;
+    std::string command = "nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2\n";
+    FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe)
+    {
+        std::cerr << "Failed to rread wifi ssid";
+        return "NO PASS";    
+    }
+
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr)
+    {
+        ssid += buffer;
+    }
+
+    if (!ssid.empty() && ssid.back() == '\n') 
+    {
+        ssid.erase(ssid.size() - 1); // Remove the last character
+    }
+
+    return ssid;
+}
+
+/**
+ * @brief Retrieves the passphrase.
+ */
+std::string ChipWrapper::getPassphrase()
+{
+    char buffer[128];
+    std::string passphrase;
+    std::string command = "sudo nmcli -s -g 802-11-wireless-security.psk connection show preconfigured";
+    FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe)
+    {
+        std::cerr << "Failed to read wifi ssid";
+        return "NO PASSPHRASE";    
+    }
+
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr)
+    {
+        passphrase += buffer;
+    }
+
+    std::cout << "pass: " << passphrase << std::endl;
+   
+    if (!passphrase.empty() && passphrase.back() == '\n') 
+    {
+        passphrase.erase(passphrase.size() - 1); // Remove the last character
+
+    }
+    
+    return passphrase;
 }
--- ChipWrapper.h
+++ ChipWrapper.h
@@ -6,14 +6,48 @@
 class ChipWrapper
 {
 public:
+    /**
+     * @brief Constructor for the ChipWrapper class.
+     */
     ChipWrapper();
+
+    /**
+     * @brief Destructor for the ChipWrapper class.
+     */
     ~ChipWrapper();
 
+    /**
+     * @brief Initializes the chip.
+     * @return True if initialization was successful, false otherwise.
+     */
     bool Initialize();
 
-    bool SendCommand(const int deviceID, const std::string& command);
+    /**
+     * @brief Sends a command to the specified device.
+     * @param deviceID The ID of the device to send the command to.
+     * @return True if the command was sent successfully, false otherwise.
+     */
+    bool CommissionDevice(const int deviceID);
 
+    /**
+     * @brief Gets the status of the specified device.
+     * @param deviceID The ID of the device.
+     * @return A string representing the device status.
+     */
     std::string GetDeviceStatus(const int deviceID);
+
+private:
+    /**
+     * @brief Retrieves the SSID.
+     * @return The SSID as a string.
+     */
+    std::string getSSID();
+
+    /**
+     * @brief Retrieves the passphrase.
+     * @return The passphrase as a string.
+     */
+    std::string getPassphrase();
 };
 
 #endif  // CHIP_WRAPPER_H
